9df7ff294744ae86ee57fcdf927290ff1827b054
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-03-08 14:47:51 +0530

    Time: 3 ms (19.34%), Space: 85 MB (91.34%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int i, p=0, n=0, e=0;
+        for (i=0; i<nums.length-1; i++){
+            if ((nums[i+1]-nums[i])> 0) p++;
+            else if ((nums[i+1]-nums[i])< 0) n++;
+            else e++;
+        }
+
+        if ((p!=0 && n==0) || (n!=0 && p==0) || (p==0 && n==0 && e==(nums.length-1))) return true;
+        else return false;
+    }
+}
