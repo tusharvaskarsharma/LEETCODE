71cf9aa69f269d1575f371a5805e8443a548df33
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-07-20 21:56:59 +0530

    Time: 2 ms (66.44%), Space: 85.3 MB (22.06%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,13 +1,21 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int i, p=0, n=0, e=0;
-        for (i=0; i<nums.length-1; i++){
-            if ((nums[i+1]-nums[i])> 0) p++;
-            else if ((nums[i+1]-nums[i])< 0) n++;
-            else e++;
-        }
+        // int i, p=0, n=0, e=0;
+        // for (i=0; i<nums.length-1; i++){
+        //     if ((nums[i+1]-nums[i])> 0) p++;
+        //     else if ((nums[i+1]-nums[i])< 0) n++;
+        //     else e++;
+        // }
 
-        if ((p!=0 && n==0) || (n!=0 && p==0) || (p==0 && n==0 && e==(nums.length-1))) return true;
-        else return false;
+        // if ((p!=0 && n==0) || (n!=0 && p==0) || (p==0 && n==0 && e==(nums.length-1))) return true;
+        // else return false;
+
+        boolean inc = true, dec = true;
+        for(int i=0; i<nums.length-1; i++){
+            if (nums[i] > nums[i+1]) inc = false;
+            if (nums[i] < nums[i+1]) dec = false;
+        }
+        return inc || dec;
     }
+
 }
