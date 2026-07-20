8ea74296c1a3148e50f7ff17d9b17488ba8f7613
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-07-20 22:00:04 +0530

    Time: 1 ms (96.1%), Space: 85.1 MB (61.32%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -14,6 +14,8 @@ class Solution {
         for(int i=0; i<nums.length-1; i++){
             if (nums[i] > nums[i+1]) inc = false;
             if (nums[i] < nums[i+1]) dec = false;
+
+            if ((inc == false) && (dec == false)) return false;
         }
         return inc || dec;
     }
