0155879b974275a30605067d62ddd23f6b414cf5
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-07-22 17:38:32 +0530

    Time: 0 ms (100%), Space: 43.2 MB (90.87%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1; i>=0; i--){
+            if (digits[i] != 9){
+                digits[i] = digits[i]+1;
+                return digits;
+            } else digits[i] = 0;
+        }
+        int arr[] = new int[digits.length+1];
+        for(int i=0; i<arr.length; i++){
+            if(i==0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+        return arr;
+    }
+}
