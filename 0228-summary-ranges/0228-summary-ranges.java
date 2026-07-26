1a0f4dbb011ea815cd012f53c2e12fb3ade61077
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-07-27 01:41:29 +0530

    Time: 2 ms (92.52%), Space: 43.3 MB (10.47%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+
+        List<String> ans = new ArrayList<>();
+        int n = nums.length;
+        int i = 0;
+
+        while (i < n) {
+            int start = i;
+
+            while (i + 1 < n && nums[i + 1] == nums[i] + 1) i++;
+            if (start == i) ans.add(String.valueOf(nums[start]));
+            else ans.add(nums[start] + "->" + nums[i]);
+            i++;
+        }
+        return ans;
+    }
+}
