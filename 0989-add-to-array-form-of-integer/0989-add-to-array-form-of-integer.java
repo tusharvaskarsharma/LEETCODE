19ce69196a1f296841b8211e28da3a71bf4f6146
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-07-28 00:08:02 +0530

    Time: 3 ms (74.5%), Space: 47.8 MB (49.33%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> ans = new ArrayList<>();
+        int i = num.length - 1;
+        while (i >= 0 || k > 0) {
+            if (i >= 0) k += num[i];
+            ans.add(k % 10);
+            k /= 10;
+            i--;
+        }
+
+        while (i >= 0) {
+            ans.add(num[i]);
+            i--;
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+}
