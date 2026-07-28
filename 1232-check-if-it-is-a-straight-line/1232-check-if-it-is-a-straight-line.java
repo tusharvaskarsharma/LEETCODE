89a551425471725cd5514504289d11801220ce5c
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-07-28 20:45:14 +0530

    Time: 0 ms (100%), Space: 45.3 MB (24.17%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -1,18 +1,16 @@
 class Solution {
-    public boolean checkStraightLine(int[][] coordinates) {
-        int x1 = coordinates[0][0];
-        int y1 = coordinates[0][1];
-        int x2 = coordinates[1][0];
-        int y2 = coordinates[1][1];
+    public boolean checkStraightLine(int[][] c) {
+        int x1 = c[0][0];
+        int y1 = c[0][1];
+        int x2 = c[1][0];
+        int y2 = c[1][1];
 
-        for (int i = 2; i < coordinates.length; i++) {
-            int x = coordinates[i][0];
-            int y = coordinates[i][1];
+        for (int i = 2; i < c.length; i++) {
+            int x = c[i][0];
+            int y = c[i][1];
 
-            if ((y2 - y1) * (x - x1) != (y - y1) * (x2 - x1))
-                return false;
+            if ((y2 - y1) * (x - x1) != (y - y1) * (x2 - x1)) return false;
         }
-
         return true;
     }
 }
