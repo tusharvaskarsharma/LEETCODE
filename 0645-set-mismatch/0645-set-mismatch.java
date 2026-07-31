9993a2723a84135acb87881ffbdac42720a90332
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-08-01 04:55:46 +0530

    Time: 2 ms (94.59%), Space: 47.8 MB (35.72%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] freq = new int[nums.length + 1];
+
+        for (int num : nums) freq[num]++;
+
+        int duplicate = -1;
+        int missing = -1;
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (freq[i] == 2) duplicate = i;
+            if (freq[i] == 0) missing = i;
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
