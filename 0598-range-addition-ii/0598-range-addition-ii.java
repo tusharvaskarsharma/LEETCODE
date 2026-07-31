6e11198ea99396308e7018998321bec2de8c14da
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-07-31 22:11:53 +0530

    Time: 0 ms (100%), Space: 48.7 MB (20.36%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.java
+++ 0598-range-addition-ii/0598-range-addition-ii.java
@@ -1,9 +1,8 @@
 class Solution {
     public int maxCount(int m, int n, int[][] ops) {
         if(ops.length == 0) return m*n;
-        int minr = 100000;
-        int minc = 100000;
-        int count = 0;
+        int minr = m;
+        int minc = n;
         for (int[] op : ops) {
             minr = Math.min(op[0], minr);
             minc = Math.min(op[1], minc);
