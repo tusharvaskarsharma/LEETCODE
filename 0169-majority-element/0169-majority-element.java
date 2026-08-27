cfb791195c3fffb791564485ce4ce90c8b5d4eb3
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-08-27 11:05:00 +0530

    Time: 6 ms (51.85%), Space: 55.8 MB (17.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,12 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        if (nums.length%2==0){
-            int mid = nums.length/2;
-            return nums[mid];
-        }else {
-            int mid = nums.length/2;
-            return nums[mid];
-        }
+        int mid = nums.length/2;
+        return nums[mid];
     }
 }
