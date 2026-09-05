741ed7355dead2a14973b5a925f1a2b24b02c63d
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-09-05 21:04:22 +0530

    Time: 4 ms (88.55%), Space: 126.7 MB (71.37%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -6,13 +6,6 @@ class Solution {
         int max[] = new int[n];
         int min[] = new int[n];
 
-        for (i=0; i<n; i++){
-            if (nums[i]>=maxn){
-                maxn = nums[i];
-            }
-            max[i] = maxn;
-        }
-
         for (i=n-1; i>=0; i--){
             if (nums[i]<=minn){
                 minn = nums[i];
@@ -20,8 +13,11 @@ class Solution {
             min[i] = minn;
         }
 
-        // 0 se n-1 tak check karenge taaki sabse PEHLA (first) stable index mile
-        for (i = 0; i < n; i++) {
+        for (i=0; i<n; i++){
+            if (nums[i]>=maxn){
+                maxn = nums[i];
+            }
+            max[i] = maxn;
             if (max[i] - min[i] <= k) { 
                 return i; // Pehla stable index milte hi yahan se return ho jao
             }
