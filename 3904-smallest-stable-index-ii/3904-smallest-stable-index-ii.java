b8d38fa24d6ba2e69351d60c6928fc5b727f3f44
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-09-05 21:02:51 +0530

    Time: 6 ms (45.8%), Space: 126.3 MB (91.98%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int i, n = nums.length;
+        int maxn = Integer.MIN_VALUE; 
+        int minn = Integer.MAX_VALUE; 
+        int max[] = new int[n];
+        int min[] = new int[n];
+
+        for (i=0; i<n; i++){
+            if (nums[i]>=maxn){
+                maxn = nums[i];
+            }
+            max[i] = maxn;
+        }
+
+        for (i=n-1; i>=0; i--){
+            if (nums[i]<=minn){
+                minn = nums[i];
+            }
+            min[i] = minn;
+        }
+
+        // 0 se n-1 tak check karenge taaki sabse PEHLA (first) stable index mile
+        for (i = 0; i < n; i++) {
+            if (max[i] - min[i] <= k) { 
+                return i; // Pehla stable index milte hi yahan se return ho jao
+            }
+        }
+
+        return -1; // Agar poore loop mein koi stable index nahi mila
+
+    }
+}
