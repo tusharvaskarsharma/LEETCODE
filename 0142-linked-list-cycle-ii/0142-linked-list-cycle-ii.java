3b9a52cc06b4b9e52309cfa157fce02965558608
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-09-10 10:38:08 +0530

    Time: 0 ms (100%), Space: 46.9 MB (13.58%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head, prev = null;
+        Boolean isDect = false;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow){
+                isDect = true;
+                break;
+            }
+        }
+
+        if (isDect){
+            slow = head;
+            while (slow != fast){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+        else return null;
+    }
+}
