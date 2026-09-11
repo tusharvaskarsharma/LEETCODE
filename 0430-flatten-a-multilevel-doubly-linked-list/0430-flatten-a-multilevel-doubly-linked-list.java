ae8998766ffb244300c9375797fd6547f3c33dc2
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-09-11 21:07:39 +0530

    Time: 1 ms (43.75%), Space: 43.4 MB (74.57%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -8,27 +8,41 @@ class Node {
 };
 */
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
 class Solution {
     public Node flatten(Node head) {
-        if (head == null) return head;
+        if(head==null) return head;
 
-        Node cur = head;
-        while (cur != null){
-            if (cur.child != null){
-                Node nxt = cur.next;
-                cur.next = flatten(cur.child);
-                cur.next.prev = cur;
-                cur.child = null;
+        Node curr=head;
+        while(curr!=null){
+            if(curr.child!=null){
+                //flatten the child node
+               Node next=curr.next;
+               curr.next=flatten(curr.child);
+               curr.next.prev=curr;
+               curr.child=null;
+            
+                //find node
+                while(curr.next!=null) curr=curr.next;
 
-                while(cur.next != null) cur = cur.next;
-
-                if (nxt != null){
-                    cur.next = nxt;
-                    nxt.prev = cur;
+                // attach tail with next ptr
+                if(next!=null){
+                    curr.next=next;
+                    next.prev=curr;
                 }
             }
-            cur = cur.next;
+            curr=curr.next;
         }
+        
         return head;
     }
 }
