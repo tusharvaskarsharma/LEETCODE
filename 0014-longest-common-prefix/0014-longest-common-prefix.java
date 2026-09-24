fee16be039ea8973f53d5bcb9da3b3e3a96c4414
TUSHAR VASKAR SHARMA <tusharvaskarsharma@gmail.com>
2026-09-24 19:49:13 +0530

    Time: 1 ms (59.95%), Space: 43.5 MB (8.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,7 +6,6 @@ class Solution {
             for (int j = 1; j < strs.length; j++) 
                 if (i == strs[j].length() || strs[j].charAt(i) != ch) 
                     return first.substring(0, i);
-            
         }
         return first;
     }
